23e3976d165f6b40d8194659b2e10e5c7f8f6e9a
--global <leonardo@vilelapinheiro.com>
2020-07-23 18:36:22 -0300

    Post 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use messages_actix::MessageApp;
 use std::io::Result;
 
-fn main() -> std::io::Result<_> {
+fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     
     MessageApp::new(8080)
-        .run();
+        .run()
 }
 
