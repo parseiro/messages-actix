4f5f22cbb327a81ecd9445accbba8e60c61080c3
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2020-07-25 11:54:29 -0300

    Just formatting the file users.rs

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -5,18 +5,23 @@ use crate::errors::AppError;
 use crate::routes::convert;
 use actix_web::{web, HttpResponse};
 use futures::Future;
-//use diesel::r2d2::Pool;
 
-//mod models;
-//use models;
+pub fn configure(cfg: &mut web::ServiceConfig) {
+    cfg
+        .service(web::resource("/users").route(web::post().to_async(create_user)))
+        .service(web::resource("/users/find/{name}").route(web::get().to_async(find_user)))
+        .service(web::resource("/users/{id}").route(web::get().to_async(get_user)));
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct UserInput {
     username: String,
 }
 
-fn create_user(item: web::Json<UserInput>, pool: web::Data<Pool>)
--> impl Future<Item = HttpResponse, Error = AppError> {
+fn create_user(
+    item: web::Json<UserInput>,
+    pool: web::Data<Pool>,
+) -> impl Future<Item = HttpResponse, Error = AppError> {
     web::block(move || {
         let conn = &pool.get().unwrap();
         let username = item.into_inner().username;
@@ -25,8 +30,10 @@ fn create_user(item: web::Json<UserInput>, pool: web::Data<Pool>)
     .then(convert)
 }
 
-fn find_user(name: web::Path<String>, pool: web::Data<Pool>)
--> impl Future<Item = HttpResponse, Error = AppError> {
+fn find_user(
+    name: web::Path<String>,
+    pool: web::Data<Pool>,
+) -> impl Future<Item = HttpResponse, Error = AppError> {
     web::block(move || {
         let conn = &pool.get().unwrap();
         let name = name.into_inner();
@@ -37,8 +44,10 @@ fn find_user(name: web::Path<String>, pool: web::Data<Pool>)
 }
 
 
-fn get_user(user_id: web::Path<i32>, pool: web::Data<Pool>)
--> impl Future<Item = HttpResponse, Error = AppError> {
+fn get_user(
+    user_id: web::Path<i32>,
+    pool: web::Data<Pool>,
+) -> impl Future<Item = HttpResponse, Error = AppError> {
     web::block(move || {
         let conn = &pool.get().unwrap();
         let id = user_id.into_inner();
@@ -46,11 +55,4 @@ fn get_user(user_id: web::Path<i32>, pool: web::Data<Pool>)
         models::find_user(conn, key)
     })
     .then(convert)
-}
-
-pub fn configure(cfg: &mut web::ServiceConfig) {
-    cfg
-        .service(web::resource("/users").route(web::post().to_async(create_user)))
-        .service(web::resource("/users/find/{name}").route(web::get().to_async(find_user)))
-        .service(web::resource("/users/{id}").route(web::get().to_async(get_user)));
 }
