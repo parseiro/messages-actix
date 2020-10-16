826b82d0b496948ddfcedd901c35af7c595d6c33
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2020-10-16 19:08:48 -0300

    Checkpoint

--- src/main.rs
+++ src/main.rs
@@ -29,8 +29,8 @@ use actix_web::{HttpServer, App};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-
     std::env::set_var("RUST_LOG", "actix_web=info");
+
     env_logger::init();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
